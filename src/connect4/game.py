2f34af645e2194a5621d554874a85dcd6a98cb9e
Matthieu Mbarga Owona <matthieu.mbarga_owona.sita@aero>
2024-12-24 19:06:38 +0100

    changed name of board to game in .py files

---
+++ src/connect4/game.py
@@ -0,0 +1,84 @@
+from enum import Enum, auto
+import numpy as np
+from typing import Self
+
+NUMBER_OF_ROWS = 6
+NUMBER_OF_COLUMNS = 7 
+RECQUIRED_ALIGNED_PAWNS_TO_WIN = 4
+
+
+class Color(Enum):
+    RED = auto()
+    YELLOW = auto()
+    EMPTY = auto()
+
+class Game:
+
+    def __init__(self: Self, board: np.ndarray, player_turn: Color) -> Self:
+        self.board = board
+        self.player_turn = player_turn
+
+    @staticmethod
+    def initialize_board() -> Self:
+        return Game(board = np.full((NUMBER_OF_ROWS, NUMBER_OF_COLUMNS), Color.EMPTY), 
+                     player_turn = Color.YELLOW)
+    
+    def switch_player_turn(self: Self) -> Self:
+        self.player_turn = Color.RED if self.player_turn == Color.YELLOW else Color.YELLOW
+        return self
+
+    def get_position_of_highest_pawn(self: Self, column: int) -> int:
+        assert 0 <= column < NUMBER_OF_COLUMNS
+        if np.all(self.board[:, column] == Color.EMPTY):
+            return NUMBER_OF_ROWS # convention: if no pawn in a column, lowest pawn is "in the ground"
+        else:
+            return np.where(self.board[:, column] != Color.EMPTY)[0][0]
+    
+    def play(self: Self, column: int, player_turn: Color) -> Self:
+        assert 0 <= column < NUMBER_OF_COLUMNS
+        assert player_turn == self.player_turn
+        position_of_highest_pawn = self.get_position_of_highest_pawn(column)
+        assert 0 < position_of_highest_pawn 
+        self.board[position_of_highest_pawn-1][column] = player_turn
+        self.switch_player_turn()
+        return self
+    
+    def check_horizontal_victory(self: Self, i: int, j: int, player_color: Color): #(moving to the right)
+        for k in range(RECQUIRED_ALIGNED_PAWNS_TO_WIN):
+            if j+k > NUMBER_OF_COLUMNS-1: # reached end of the board 
+                return False
+            if self.board[i][j+k] != player_color: # found a different color than player's color
+                return False
+        return True
+
+    def check_vertical_victory(self: Self, i: int, j: int, player_color: Color): # (moving to the top)
+        for k in range(RECQUIRED_ALIGNED_PAWNS_TO_WIN):
+            if i-k < 0: # reached end of the board 
+                return False
+            if self.board[i-k][j] != player_color: # found a different color than player's color
+                return False
+        return True
+
+    def check_diagonal_victory(self: Self, i: int, j: int, player_color: Color): # (moving to the top/right)
+        for k in range(RECQUIRED_ALIGNED_PAWNS_TO_WIN):
+            if i-k > NUMBER_OF_ROWS-1 or j+k > NUMBER_OF_COLUMNS-1: # reached end of the board
+                return False
+            if self.board[i-k][j+k] != player_color: # found a different color than player's color
+                return False
+        return True
+    
+    def check_victory(self: Self, player_color: Color) -> Self:
+        for i in range(NUMBER_OF_ROWS):
+            for j in range(NUMBER_OF_COLUMNS):
+                horizontal_victory = self.check_horizontal_victory(i, j, player_color)
+                vertical_victory = self.check_vertical_victory(i, j, player_color)
+                diagonal_victory = self.check_diagonal_victory(i, j, player_color)
+                if horizontal_victory or vertical_victory or diagonal_victory:
+                    return True
+        return False
+    
+    
+                    
+
+
+        
---
+++ tests/unit_tests/test_game.py
@@ -0,0 +1,102 @@
+# tests/test_board.py
+import pytest
+import numpy as np
+import copy 
+
+from connect4.game import Game, Color, NUMBER_OF_ROWS, NUMBER_OF_COLUMNS, RECQUIRED_ALIGNED_PAWNS_TO_WIN
+
+@pytest.fixture
+def game():
+    game = Game(board = np.full((NUMBER_OF_ROWS, NUMBER_OF_COLUMNS), Color.EMPTY), 
+                     player_turn = Color.YELLOW) 
+    return game
+
+def test_initialize_board():
+    game = Game.initialize_board()
+    assert np.all(game.board == Color.EMPTY)
+    assert game.player_turn == Color.YELLOW
+
+def test_switch_player_turn(game): 
+    game.switch_player_turn()
+    assert game.player_turn == Color.RED
+    game.switch_player_turn()
+    assert game.player_turn == Color.YELLOW
+
+def test_get_position_of_highest_pawn(game):
+    assert game.get_position_of_highest_pawn(column = 0) == NUMBER_OF_ROWS 
+    game.board[0,0] = Color.RED
+    assert game.get_position_of_highest_pawn(column = 0) == 0
+    game.board[NUMBER_OF_ROWS-1,1] = Color.YELLOW
+    assert game.get_position_of_highest_pawn(column = 1) == NUMBER_OF_ROWS-1
+
+def test_play(game):
+    column = 0
+    game.play(column, player_turn = Color.YELLOW)
+    assert game.player_turn == Color.RED
+    for i in range(NUMBER_OF_ROWS-1):
+        assert game.board[i,column] == Color.EMPTY
+    assert game.board[NUMBER_OF_ROWS-1,column] == Color.YELLOW
+    game.play(column, player_turn = Color.RED)
+    assert game.board[NUMBER_OF_ROWS-2,column] == Color.RED
+    assert game.board[NUMBER_OF_ROWS-1,column] == Color.YELLOW
+
+def test_check_horizontal_victory(game):
+    for i in range(NUMBER_OF_ROWS):
+        for j in range(NUMBER_OF_COLUMNS):
+            assert not game.check_horizontal_victory(i, j, player_color=Color.YELLOW)
+            assert not game.check_horizontal_victory(i, j, player_color=Color.RED)
+    for j in range(RECQUIRED_ALIGNED_PAWNS_TO_WIN):
+        game.board[NUMBER_OF_ROWS-1,j] = Color.YELLOW
+    assert game.check_horizontal_victory(NUMBER_OF_ROWS-1, 0, player_color=Color.YELLOW)
+    assert not game.check_horizontal_victory(NUMBER_OF_ROWS-1, 0, player_color=Color.RED)
+    assert not game.check_horizontal_victory(NUMBER_OF_ROWS-1, 1, player_color=Color.YELLOW)
+    assert not game.check_horizontal_victory(NUMBER_OF_ROWS-1, NUMBER_OF_COLUMNS, player_color=Color.YELLOW)
+
+def test_check_vertical_victory(game):
+    game = Game(board = np.full((NUMBER_OF_ROWS, NUMBER_OF_COLUMNS), Color.EMPTY), 
+                     player_turn = Color.YELLOW) 
+    for i in range(NUMBER_OF_ROWS):
+        for j in range(NUMBER_OF_COLUMNS):
+            assert not game.check_vertical_victory(i, j, player_color=Color.YELLOW)
+            assert not game.check_vertical_victory(i, j, player_color=Color.RED)
+    for i in range(RECQUIRED_ALIGNED_PAWNS_TO_WIN):
+        game.board[NUMBER_OF_ROWS-1-i,0] = Color.YELLOW
+    assert game.check_vertical_victory(NUMBER_OF_ROWS-1, 0, player_color=Color.YELLOW)
+    assert not game.check_vertical_victory(NUMBER_OF_ROWS-1, 0, player_color=Color.RED)
+    assert not game.check_vertical_victory(NUMBER_OF_ROWS-1, 1, player_color=Color.YELLOW)
+    assert not game.check_vertical_victory(NUMBER_OF_ROWS-1, NUMBER_OF_COLUMNS-1, player_color=Color.YELLOW)
+
+def test_check_diagonal_victory(game):
+    for i in range(NUMBER_OF_ROWS):
+        for j in range(NUMBER_OF_COLUMNS):
+            assert not game.check_diagonal_victory(i, j, player_color=Color.YELLOW)
+            assert not game.check_diagonal_victory(i, j, player_color=Color.RED)
+    for k in range(RECQUIRED_ALIGNED_PAWNS_TO_WIN):
+        game.board[NUMBER_OF_ROWS-1-k,k] = Color.YELLOW
+    assert game.check_diagonal_victory(NUMBER_OF_ROWS-1, 0, player_color=Color.YELLOW)
+    assert not game.check_diagonal_victory(NUMBER_OF_ROWS-1, 0, player_color=Color.RED)
+    assert not game.check_diagonal_victory(NUMBER_OF_ROWS-1, 1, player_color=Color.YELLOW)
+    assert not game.check_diagonal_victory(NUMBER_OF_ROWS-1, NUMBER_OF_COLUMNS-1, player_color=Color.YELLOW)
+
+def test_check_victory(game):
+    assert not game.check_victory(player_color = Color.YELLOW)
+    assert not game.check_victory(player_color = Color.RED)
+
+    new_game = copy.deepcopy(game)
+    for j in range(RECQUIRED_ALIGNED_PAWNS_TO_WIN):
+        new_game.board[NUMBER_OF_ROWS-1,j] = Color.YELLOW
+    assert new_game.check_victory(player_color = Color.YELLOW)
+    assert not new_game.check_victory(player_color = Color.RED)
+
+    new_game = copy.deepcopy(game)
+    for i in range(RECQUIRED_ALIGNED_PAWNS_TO_WIN):
+        new_game.board[NUMBER_OF_ROWS-1-i,0] = Color.YELLOW
+    assert new_game.check_victory(player_color = Color.YELLOW)
+    assert not new_game.check_victory(player_color = Color.RED)
+
+    new_game = copy.deepcopy(game)
+    for k in range(RECQUIRED_ALIGNED_PAWNS_TO_WIN):
+        new_game.board[NUMBER_OF_ROWS-1-k,k] = Color.YELLOW
+    assert new_game.check_victory(player_color = Color.YELLOW)
+    assert not new_game.check_victory(player_color = Color.RED)
+
