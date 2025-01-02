14b767f0a4aa13309663af45dbca391b7a313d44
Matthieu Mbarga Owona <matthieu.mbarga_owona.sita@aero>
2025-01-02 11:12:16 +0100

    new functions to compute value of a game_state or game_state_tree

--- src/connect4/game_state_tree.py
+++ src/connect4/game_state_tree.py
@@ -1,6 +1,7 @@
 from typing import Self, List
+import copy
 
-from src.connect4.game_state import GameState
+from src.connect4.game_state import GameState, Color
 
 class GameStateTree:
 
@@ -8,14 +9,20 @@ class GameStateTree:
                  current_game_state: GameState = None, 
                  children_game_state_trees: List['GameStateTree'] = None) -> None:
 
+        if current_game_state is None: 
+            raise ValueError('Error : game_state should be specified.')
+        if children_game_state_trees is None:
+            children_game_state_trees = []
+
         self.current_game_state = current_game_state
         self.children_game_state_trees = children_game_state_trees
+        self.value = None
     
     def is_leaf(self: Self) -> bool:
         return self.children_game_state_trees == []
     
-    def get_all_nodes(self: Self) -> List['GameStateTree']:
-        all_nodes = []
+    def get_all_subtrees(self: Self) -> List['GameStateTree']:
+        all_subtrees = []
         game_state_trees_to_explore = [{'game_state_tree': self, 
                                         'father' : None, 
                                         'depth': 0}]
@@ -23,7 +30,7 @@ class GameStateTree:
         while game_state_trees_to_explore:
             currently_explored_game_state_tree = game_state_trees_to_explore.pop(0)
     
-            all_nodes.append(currently_explored_game_state_tree)
+            all_subtrees.append(currently_explored_game_state_tree)
             
             for next_game_state in currently_explored_game_state_tree['game_state_tree'].children_game_state_trees:
                 if next_game_state not in explored_game_states:
@@ -32,21 +39,21 @@ class GameStateTree:
                                                         'depth': currently_explored_game_state_tree['depth']+1})
                     explored_game_states.add(next_game_state)  
 
-        return all_nodes
+        return all_subtrees
     
     def get_all_leaves(self: Self) -> List['GameStateTree']:
-        return [node['game_state_tree'] for node in self.get_all_nodes() if node['game_state_tree'].is_leaf()]
+        return [node['game_state_tree'] for node in self.get_all_subtrees() if node['game_state_tree'].is_leaf()]
         
     def get_depth(self: Self, node: 'GameStateTree') -> int:
-        for some_node in self.get_all_nodes():
+        for some_node in self.get_all_subtrees():
             if some_node['game_state_tree'] is node:
                 return some_node['depth']
             
     def get_height(self: Self) -> int:
-        return max([node['depth'] for node in self.get_all_nodes()])
+        return max([node['depth'] for node in self.get_all_subtrees()]) + 1
 
     def get_father(self: Self, node: 'GameStateTree') -> 'GameStateTree':
-        for some_node in self.get_all_nodes():
+        for some_node in self.get_all_subtrees():
             if some_node['game_state_tree'] is node:
                 return some_node['father']
     
@@ -62,13 +69,36 @@ class GameStateTree:
                 leaf.extend_from_root()
         return self
     
-    @staticmethod
-    def compute_game_state_value(game_state: GameState, value_function: function, depth: int) -> float:
-        game_state_tree = GameStateTree(game_state, [])
-        game_state_tree.extend(depth)
-        for depth in 
-
-
-            
-        return 0
+    def compute_game_state_tree_value(self: Self, value_function: 'function', player: Color) -> float:
+        all_subtrees = [subtree['game_state_tree'] for subtree in self.get_all_subtrees()]
+        while self.value is None:
+            for subtree in all_subtrees:
+                subtree_children_values = [subtree_child.value for subtree_child in subtree.children_game_state_trees]
+                if subtree.is_leaf():
+                    subtree.value = value_function(subtree.current_game_state, player)
+                elif all([isinstance(subtree_children_value, float) for subtree_children_value in subtree_children_values]):
+                    subtree.value = max(subtree_children_values) if subtree.current_game_state.player_turn == player else min(subtree_children_values)
+                else:
+                    pass   
+                if subtree==self:
+                    print('self')
+                else:
+                    print('child')
+                print(subtree.current_game_state.board)
+                print('subtree: ', subtree.value)
+                print('subtree: ', subtree.current_game_state.player_turn)
+                print('self: ', self.value)
+      
+        return self.value
     
+    @staticmethod
+    def compute_game_state_value(game_state: GameState, value_function: 'function', player: Color, depth) -> float:
+        game_state_tree = GameStateTree(game_state)
+        game_state_tree.extend(depth)
+
+        return game_state_tree.compute_game_state_tree_value(value_function, player)
+        
+    
+# je parcours les noeuds tant que le pere n'a pas de valeur
+# si j'ai un noeud sans enfant, ou si tous ses enfants ont une valeur, je calcule la valeur de mon noeud
+# on continue jusqu'à tout avoir
